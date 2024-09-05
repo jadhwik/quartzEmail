f984db65f1fb18823c343657f4a2efaf5a350962
jadhwik <adhwikjagadeesh@gmail.com>
2024-09-05 23:20:39 +0530

    EmailConfig modified

--- src/main/java/co/instio/EmailConfig.java
+++ src/main/java/co/instio/EmailConfig.java
@@ -4,16 +4,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import java.util.Properties;
 
 @Configuration
 public class EmailConfig {
 
     private String fromMailId;
 
-    @Bean
-    public JavaMailSender javaMailSender() {
 
-        return new JavaMailSenderImpl();
+
+        @Bean
+        public JavaMailSender getJavaMailSender() {
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setHost("smtp.gmail.com");
+            mailSender.setPort(587); // Use 465 for SSL
+
+            mailSender.setUsername("adhwikjagadeesh@gmail.com");
+            mailSender.setPassword("svuu knat irbi oyqw");
+
+            Properties props = mailSender.getJavaMailProperties();
+            props.put("mail.transport.protocol", "smtp");
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.starttls.required", "true");
+            props.put("mail.smtp.ssl.enable", "false"); // true for SSL (port 465)
+            props.put("mail.debug", "true");
+
+            return mailSender;
+        }
     }
 
-}
